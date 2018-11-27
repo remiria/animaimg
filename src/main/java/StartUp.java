78a54f9a80b4aef79ab67f09c72e3ef119f2fd47
Liuzx <liuzxnet@163.com>
2018-11-27 09:18:12 +0800

    format code

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -4,7 +4,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
 
-
 /**
  * @author Remiria
  * @version 1.0
@@ -12,13 +11,12 @@ import java.util.List;
  */
 public class StartUp {
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String bcyUrl = "https://bcy.net/item/detail/6628019396625301764";
-        String result = NetUtils.getContent(bcyUrl);
-        List<String> list = NetUtils.getImgSrc(URLDecoder.decode(result,"utf-8"));
-        for (String img : list){
-            System.out.println(img);
-            System.out.println(img);
-        }
-    }
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String bcyUrl = "https://bcy.net/item/detail/6628019396625301764";
+		String result = NetUtils.getContent(bcyUrl);
+		List<String> list = NetUtils.getImgSrc(URLDecoder.decode(result, "utf-8"));
+		for (String img : list) {
+			System.out.println(img);
+		}
+	}
 }
